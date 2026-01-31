248b9d874bfe5c614133cb1bce0ea164a5dfb349
Strandix-system <admin@strandixsystem.com>
2026-01-31 21:09:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,15 +17,14 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: [
+      "https://main.d13qtkfj0o1mlk.amplifyapp.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }));
 
-// {
-//     origin: [
-//       "https://main.d13qtkfj0o1mlk.amplifyapp.com",
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   }
 app.use(compression());
 
 const server = http.createServer(app);

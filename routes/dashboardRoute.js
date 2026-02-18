7c59410380eebc1c0c4f259327de7853231ae92b
Strandix-system <admin@strandixsystem.com>
2026-02-18 16:26:51 +0530

    Update dashboardRoute.js

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { tokenVerification } from "../middleware/auth.js";
 import { getDashboardStats } from "../src/admin/Dashboard/controller.js";
+import { allowRoles } from "../middleware/permission.js";
 const router = express.Router();
+
 
 // Dashboard Stats (SuperAdmin + Admin)
 router.get(
     "/stats",
     tokenVerification,
+     allowRoles("superadmin", "admin"),
     getDashboardStats
 );
 

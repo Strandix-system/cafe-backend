2958d8f8c382d4a31d10064d56f94d8aad6ffc73
Tanishq34 <tanishqrimjha31@gmail.com>
2026-02-19 17:55:31 +0530

    Remove dotenv import and configuration
    Removed dotenv configuration from email.js

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,4 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const sendResetEmail = async (email, resetLink) => {
   const transporter = nodemailer.createTransport({

c7c16f7c55d881d51c7fa24ae62bb35d37f04180
Tanishq34 <tanishqrimjha31@gmail.com>
2026-02-19 13:02:53 +0530

    Refactor authentication routes and controller; add password reset and change password functionality

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,11 +2,8 @@ import express from "express";
 import controller from "../src/auth/controller.js";
 import { tokenVerification } from "../middleware/auth.js";
 import { validate } from "../middleware/validate.js";
-import {
-  registerValidator,
-  loginValidator,
-  logoutValidator,
-} from "../validations/authValidation.js";
+import { registerValidator, loginValidator, logoutValidator, } from "../validations/authValidation.js";
+import { allowRoles } from "../middleware/permission.js";
 
 const router = express.Router();
 
@@ -27,6 +24,9 @@ router.post(
   controller.logout
 );
 
-router.get("/me", tokenVerification, controller.me);
+router.post("/forgot-password", controller.forgotPassword);
+router.post("/reset-password/:token", controller.resetPassword);
+router.get("/me", tokenVerification, allowRoles("admin", "superadmin"), controller.me);
+router.post("/change-password", tokenVerification, controller.changePassword);
 
 export default router;

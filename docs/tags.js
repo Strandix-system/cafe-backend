9e0d8e4641c8fe9d753f2fd2f63b7bd410cfd04f
Soninayanshree <soninayanshree08@gmail.com>
2026-04-03 11:44:15 +0530

    WIP: swagger implementation
    - basic setup for category schema

---
+++ docs/tags.js
@@ -0,0 +1,46 @@
+export const swaggerTags = [
+  {
+    name: 'Auth',
+    description: 'Authentication APIs',
+  },
+  {
+    name: 'Category',
+    description: 'Category management APIs',
+  },
+  {
+    name: 'Customer',
+    description: 'Customer management APIs',
+  },
+  {
+    name: 'Menu',
+    description: 'Menu management APIs',
+  },
+  {
+    name: 'Order',
+    description: 'Order management APIs',
+  },
+  {
+    name: 'Order Item',
+    description: 'Order item APIs',
+  },
+  {
+    name: 'Inventory',
+    description: 'Inventory management APIs',
+  },
+  {
+    name: 'Feedback',
+    description: 'Feedback APIs',
+  },
+  {
+    name: 'Profile',
+    description: 'Profile APIs',
+  },
+  {
+    name: 'Billing',
+    description: 'Billing APIs',
+  },
+  {
+    name: 'Table',
+    description: 'Table APIs',
+  },
+];
